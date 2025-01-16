a5f75219ae6bc1bdc0a6a29812b2dc6e7e65306f
lammai442 <lam.mai442@gmail.com>
2025-01-16 23:28:20 +0100

    Number 8 - Objects done

---
+++ 8-objects/javascripts/scripts.js
@@ -0,0 +1,17 @@
+// Funktion för att logga enklare. Bara att anropa log() och lägga in det som ska loggas som argument.
+function log(input) {
+    console.log(input);
+}
+
+let player = {
+    name: 'Lam',
+    age: 12,
+    isActive: true,
+    outfit: {
+        color: 'Blue',
+        size: 'M',
+        isNew: false,
+    },
+};
+
+player.name;
---
+++ 9-methods/javascripts/scripts.js
@@ -0,0 +1,4 @@
+// Funktion för att logga enklare. Bara att anropa log() och lägga in det som ska loggas som argument.
+function log(input) {
+    console.log(input);
+}
