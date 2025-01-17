a84b93a733b50570fe3e31f239aa3b31afb49612
lammai442 <lam.mai442@gmail.com>
2025-01-17 13:48:31 +0100

    Number 10 - While loop done

---
+++ 10-whileloop/javascripts/scripts.js
@@ -0,0 +1,17 @@
+function sendSignal() {
+    console.log('Help');
+}
+
+let i = 1;
+
+while (i <= 1000) {
+    sendSignal();
+    i++;
+}
+
+let j = 0;
+
+while (j <= 10) {
+    console.log(j);
+    j++;    
+}
