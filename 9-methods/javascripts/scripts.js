6c2513ef763b5b61da4954584229191423c7c6b1
lammai442 <lam.mai442@gmail.com>
2025-01-17 13:38:15 +0100

    Number 9 - Methods done

--- 9-methods/javascripts/scripts.js
+++ 9-methods/javascripts/scripts.js
@@ -1,4 +1,35 @@
-// Funktion för att logga enklare. Bara att anropa log() och lägga in det som ska loggas som argument.
-function log(input) {
-    console.log(input);
+let player = {
+    health: 100,
+    fun: 0,
+    // eatApple: function() {
+    //     console.log('eatApple');
+    //     //Man måste använda sig av this. för att kunna hämta data inom detta objekt. Annars når man inte den.
+    //     this.health += 10;
+    // },
+    // eatCandy: function() {
+    //     console.log('eatCandy');
+    //     this.health -= 10;
+    // },
+    playFun: function() {
+        console.log('playFun');
+        this.fun += 10;        
+    },
+    eat: function(food) {
+        if (food === 'Apple') {
+            this.health += 10;
+        } else if (food === 'Candy') {
+            this.health -= 10;
+        }
+    }
 }
+
+// player.eatApple();
+player.eat('Apple')
+console.log(player);
+player.eat('Candy')
+// player.eatCandy();
+console.log(player);
+player.playFun();
+console.log(player);
+
+
